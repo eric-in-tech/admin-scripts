8c145a39f1a54fa454bac884b01368b31f34dde5
Eric <70476070+eric-in-tech@users.noreply.github.com>
2025-09-23 14:42:51 -0500

    Generic-ify the filename and variables

--- remed_Drive.ps1
+++ remed_Drive.ps1
@@ -1,8 +1,11 @@
-$DriveLetter = "F"
-$Path = "\\dc2xfile01.fcs.lan\Dept"
-$Label = "Dept on DC2xFile01"
+## used as an Intune remediation script to map a specific UNC path to a specific drive letter and display a friendly label in Explorer 
+
+$DriveLetter = "A"
+$Path = "\\servername01.lan\Dept"
+$Label = "Dept on ServerName01"
 
 Write-Output "Mapping network drive $($DriveLetter): $($Path)"
 $null = New-PSDrive -PSProvider FileSystem -Name $DriveLetter -Root $Path -Description $Label -Persist -Scope global -EA Stop
 (New-Object -ComObject Shell.Application).NameSpace("$($DriveLetter):").Self.Name = $Label
+
 Get-PSDrive -name $DriveLetter

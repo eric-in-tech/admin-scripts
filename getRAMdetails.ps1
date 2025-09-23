5cd19763ae9098d8f2be84ffd65f81605345ac2c
Eric <70476070+eric-in-tech@users.noreply.github.com>
2025-09-23 14:59:40 -0500

    Update getRAMdetails.ps1

--- getRAMdetails.ps1
+++ getRAMdetails.ps1
@@ -1,3 +1,6 @@
+## short/sweet cmd to display RAM details on a Windows system
+
 Get-CimInstance Win32_PhysicalMemory | Select-Object `
     DeviceLocator, Manufacturer, @{Name="Capacity(GB)";Expression={[math]::Round($_.Capacity / 1GB, 2)}}, `
     Name, PartNumber, Model, Tag, ConfiguredClockSpeed | ft
+
